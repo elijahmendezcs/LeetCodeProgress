07914a8b821fc9388e9cc047d0bd8f4180d87b92
Elijah Mendez <elijahmendez2022@gmail.com>
2024-11-06 16:55:37 -0500

    adding deleteDuplicate solution

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -117,6 +117,25 @@ public class LinkedList {
     }
 
 
+    public ListNode deleteDuplicate(ListNode head) {
+
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+
+            if(current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+
 
 
 }
